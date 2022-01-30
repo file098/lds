057130b8ed2797e0309abfb0173201cf8463b3c9
file0 <filippodigennaro98@outlook.it>
2022-01-30 16:45:21 +0100

    added routing and minor components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WorksComponent } from './works/works.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'aboutme', component: HomeComponent },
+  { path: 'works', component: WorksComponent }, 
+  { path: 'contact', component: HomeComponent }, 
+  { path: '', redirectTo: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
