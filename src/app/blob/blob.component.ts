30f86a8722c4a996065265b275aabf3d17800501
file098 <filippodigennaro98@outlook.it>
2022-04-04 20:03:23 +0200

    dots

--- src/app/blob/blob.component.ts
+++ src/app/blob/blob.component.ts
@@ -12,31 +12,51 @@ export class BlobComponent implements OnInit, AfterViewChecked {
   xoff = 0.0;
   yoff = 0.0;
   headerWrapperHeight = 0;
+  dots = [{ x: 0, y: 0 }];
+  windowHeight = 0;
 
   constructor() { }
 
   ngAfterViewChecked(): void {
   }
 
+  setupGrid(rows: number, columns: number, density: number, height: number, width: number): void {
+    this.dots = [];
+    
+    var ratioW = height / columns * density; // ever "ratio" i need to put a dot
+    var ratioH = width / rows * density; // ever "ratio" i need to put a dot
+
+    for (var x = 0; x < width; x += ratioW) {
+      for (var y = 0; y < height; y += ratioH) {
+        this.dots.push({ x, y });
+      }
+    }
+    
+  };
+
   ngOnInit() {
 
     const header = document.querySelector('#header-wrapper');
-    if(header){ this.headerWrapperHeight = header.clientHeight; }
+    if (header) { this.headerWrapperHeight = header.clientHeight; }
 
     const sketch = (s: any) => {
-      s.setup = () => {
-        s.createCanvas(s.windowWidth, s.windowHeight).parent('canvasContainer');
-      };
 
-      s.windowResized = () => {
-        s.createCanvas(s.windowWidth, s.windowHeight).parent('canvasContainer');
+      s.setup = () => {
+        this.windowHeight = s.windowHeight - this.headerWrapperHeight;
+        s.createCanvas(s.windowWidth, s.windowHeight - this.headerWrapperHeight).parent('canvasContainer');
+        // TODO: check why the window size is wrong 
+        this.setupGrid(500, 500, 10, s.windowWidth, s.windowHeight);
       };
 
       s.draw = () => {
         s.background(255);
+
+        for (var count = 0; count < this.dots.length; count++) {
+          s.ellipse(this.dots[count].x, this.dots[count].y, 1, 1, 1);
+        }
+
         s.translate(s.width / 2, s.height / 2 - this.headerWrapperHeight);
         s.fill(60);
-
         this.xoff += 0.01;
 
         s.beginShape();
@@ -55,6 +75,7 @@ export class BlobComponent implements OnInit, AfterViewChecked {
         }
         s.endShape();
 
+        s.fill(65);
         s.beginShape();
         for (let a = 0; a < s.TWO_PI; a += 0.05) {
           let offset = s.map(s.noise(xoff, this.yoff), 0, 1, -25, 25);
@@ -65,12 +86,18 @@ export class BlobComponent implements OnInit, AfterViewChecked {
           xoff += 0.1;
         }
         s.endShape(s.CLOSE);
- 
+
         this.yoff += 0.01;
+
+      };
+
+      s.windowResized = () => {
+        s.createCanvas(s.windowWidth, s.windowHeight).parent('canvasContainer');
+        // TODO: check why the window size is wrong 
+        this.setupGrid(500, 500, 15, s.windowWidth * 2, s.windowHeight * 2);
       };
 
 
-      
     };
 
     let canvas = new p5(sketch);
